4cbffc2be36abfb5c38b852e251be3aa56ab41f7
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-05-12 09:54:49 +0700

    add bash trê

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,10 @@ import { Router } from "express";
 
 const routes = Router();
 
+// routes.use("/products", hanldeProduct...)
+// routes.use("/products", hanldeProduct...)
+// routes.use("/products", hanldeProduct...)
+// routes.use("/products", hanldeProduct...)
+// routes.use("/products", hanldeProduct...)
+
 export default routes;
