0a024fce3a3ca216a4ee652a02d96e062fff80f0
chỉnh <xuanchinhn48@gmail.com>
2025-05-22 18:32:15 +0700

    login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 export const signup = async (req, res) => {
 try {
 	const {username, password, email, full_name, phone, address} = req.body;
@@ -25,4 +26,22 @@ try {
 } catch (error) {
 	return res.status(400).json({message: "Lỗi server", error: error.message});
 }
+}
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(400).json({message: "Tài khoản không tồn tại"});
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);  
+        if(!isPasswordValid){
+            return res.status(400).json({message: "Mật khẩu không chính xác"});
+        }
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+        return res.status(200).json({message: "Đăng nhập thành công", token});
+
+    } catch (error) {
+        return res.status(400).json({message: "Lỗi server", error: error.message});
+    }
 }
