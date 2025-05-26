3e7ca588c0974ea74848e42ec96ce95c019c36a2
chỉnh <xuanchinhn48@gmail.com>
2025-05-26 21:26:58 +0700

    fix mes

--- src/constant/messages.js
+++ src/constant/messages.js
@@ -21,7 +21,12 @@ export const CATEGORY_MESSAGES = {
 
 export const AUTH_MESSAGES = {
   NOT_FOUND: "Không có tài khoản",
-  
+  EMAIL_EXISTS: "Email đã tồn tại",
+  USERNAME_EXISTS: "Tên đăng nhập đã tồn tại",
+  SIGNUP_SUCCESS: "Đăng ký thành công",
+  LOGIN_SUCCESS: "Đăng nhập thành công",
+  INVALID_PASSWORD: "Mật khẩu không đúng",
+  USER_NOT_FOUND: "Không tìm thấy người dùng",
   CREATE_SUCCESS: "Tạo tài khoản thành công",
   UPDATE_SUCCESS: "Cập nhật tài khoản thành công",
   DELETE_SUCCESS: "Xóa tài khoản thành công",
