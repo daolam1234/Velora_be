09f768780c7b9c22c0ff7e76ee8579aa004d698d
chỉnh <xuanchinhn48@gmail.com>
2025-06-11 17:08:57 +0700

    adduser

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getUser, updateUser, getDetailUser, updateUserStatus, updatePassword } from "../controllers/userController.js";
+import { getUser, updateUser, getDetailUser, updateUserStatus, updatePassword, addUser } from "../controllers/userController.js";
 
 const userRouter = Router();
 userRouter.get("/", getUser);
 userRouter.get("/:id", getDetailUser);
 userRouter.patch("/updateStatus/:id", updateUserStatus);
 userRouter.patch("/update/:id",updateUser);
-userRouter.patch("/update-password/:id", updatePassword);
+userRouter.patch("/update-password", updatePassword);
+userRouter.post("/add", addUser);
 
 export default userRouter;
