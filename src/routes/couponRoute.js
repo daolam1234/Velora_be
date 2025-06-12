baece50362c666d55ffa58b870e97768d386aca5
daolam1234 <lam15081999@gmail.com>
2025-06-13 00:54:04 +0700

    fix verifyToken

--- src/routes/couponRoute.js
+++ src/routes/couponRoute.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { createCoupon, deleteCoupon, getAllCoupons, getCouponById, updateCoupon, validateCouponForUser } from "../controllers/couponController.js";
 import { validateCouponDates } from "../middlewares/validateCouponDates.js";
-import { verifyToken } from "../middlewares/auth.js";
 const couponRouter = express.Router();
 
 couponRouter.post("/add",validateCouponDates, createCoupon);
@@ -9,6 +8,6 @@ couponRouter.put("/:id",validateCouponDates, updateCoupon);
 couponRouter.get("/", getAllCoupons);      // Lấy tất cả coupon (dùng cho admin quản lý)
 couponRouter.get("/:id", getCouponById);   // Lấy chi tiết 1 coupon
 couponRouter.delete("/:id", deleteCoupon) //xoá mềm
-couponRouter.get("/validate/:code", verifyToken, validateCouponForUser);
+couponRouter.get("/validate/:code",  validateCouponForUser);
 
 export default couponRouter;
